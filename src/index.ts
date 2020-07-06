ab88e48a66e433ed2ed691c16dd3f88c3852f7f8
leo <leo_9_t@hotmail.com>
2020-07-06 09:20:21 -0400

    FIX on env var

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import sequelize from './utils/DB';
 bootstrap()
 .then(() => sequelize.sync())
 .then(() => {
-    const server = app.listen(5000, '0.0.0.0', () => {
+    const server = app.listen(parseInt(process.env.PORT || '5000') || 5000, '0.0.0.0', () => {
         const {port, address} = server.address() as AddressInfo;
         console.log('Server listening on:','http://' + address + ':'+port);
     });
