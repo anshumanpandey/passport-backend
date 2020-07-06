a681bf642b6dd1d5147476b92e8aa9491f8b455c
leo <leo_9_t@hotmail.com>
2020-07-06 01:12:23 -0400

    ADDED db config

--- src/utils/DB.ts
+++ src/utils/DB.ts
@@ -1,9 +1,18 @@
 import { Sequelize } from "sequelize"
 import path from "path";
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '..', '..','passport-db.sqlite'),
+
+let sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, '..', '..', 'passport-db.sqlite'),
+});
+
+if (process.env.DB_DIALECT) {
+  console.log('using dialect')
+  sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USERNAME || '', process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: process.env.DB_DIALECT as "postgres" || 'postgres'
   });
+} 
 
 export default sequelize;
