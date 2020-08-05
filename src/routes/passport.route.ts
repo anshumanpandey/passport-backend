8321dad9c0870efa63e5c1d9ed1c3998e1fcb58b
leo <leo_9_t@hotmail.com>
2020-08-05 11:15:13 -0400

    ADDED editing and deleting

--- src/routes/passport.route.ts
+++ src/routes/passport.route.ts
@@ -30,10 +30,32 @@ passportRoute.post('/passport/create', jwt({ secret: process.env.JWT_SECRET || '
     trim: true
   },
 })), asyncHandler(async (req, res) => {
-  const { name } = req.body
+  const { name, id } = req.body
 
-  //@ts-expect-error
-  await PassportModel.create({ name, UserId: req.user.id })
+  if (id) {
+    await PassportModel.update({ name }, { where: { id }})
+  } else {
+    //@ts-expect-error
+    await PassportModel.create({ name, UserId: req.user.id })
+  }
+
+  res.send({ success: 'Passport created' });
+}));
+
+passportRoute.delete('/passport', jwt({ secret: process.env.JWT_SECRET || 'aa', algorithms: ['HS256'] }),validateParams(checkSchema({
+  id: {
+    in: ['body'],
+    exists: {
+      errorMessage: 'Missing field'
+    },
+    trim: true
+  },
+})), asyncHandler(async (req, res) => {
+  const { id } = req.body
+
+  const p = await PassportModel.findByPk(id)
+  if (!p) throw new ApiError("Passport not found")
+  await p.destroy();
 
   res.send({ success: 'Passport created' });
 }));
